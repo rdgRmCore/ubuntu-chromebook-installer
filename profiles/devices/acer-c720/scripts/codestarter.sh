8fe5f89706f417b7011dcfcfa609d835ec423866
Tom Preston-Werner <tom@mojombo.com>
2014-06-24 01:34:06 -0500

    Fixes and touch-logging for codestarter script.

--- profiles/devices/acer-c720/scripts/codestarter.sh
+++ profiles/devices/acer-c720/scripts/codestarter.sh
@@ -2,6 +2,7 @@
 tempbuild=`mktemp -d`
 
 echo "Installing Chrome"
+cd $tempbuild
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb
 
@@ -13,6 +14,7 @@ dpkg -i sublime-text_build-3059_amd64.deb
 echo "Installing Scratch"
 cd $tempbuild
 apt-get install scratch
+touch "scratch.done"
 
 echo "Installing ruby-install"
 cd $tempbuild
@@ -20,10 +22,12 @@ wget -O ruby-install-0.4.3.tar.gz https://github.com/postmodern/ruby-install/arc
 tar -xzf ruby-install-0.4.3.tar.gz
 cd ruby-install-0.4.3/
 make install
+touch "ruby-install.done"
 
 echo "Installing ruby"
 cd $tempbuild
 ruby-install ruby 2.1.2
+touch "ruby.done"
 
 echo "Installing chruby"
 cd $tempbuild
@@ -34,24 +38,29 @@ make install
 echo "" >> /etc/skel/.bashrc
 echo "source '/usr/local/share/chruby/chruby.sh'" >> /etc/skel/.bashrc
 echo "source '/usr/local/share/chruby/auto.sh'" >> /etc/skel/.bashrc
-echo "chruby ruby-2.1.2"
+echo "chruby ruby-2.1.2" >> /etc/skel/.bashrc
+touch "chruby.done"
 
 echo "Installing node.js"
 cd $tempbuild
 apt-get install nodejs npm
+touch "nodejs.done"
 
 echo "Installing Java"
 cd $tempbuild
 apt-get install default-jdk
+touch "java.done"
 
 echo "Installing Minecraft"
 cd $tempbuild
 add-apt-repository ppa:minecraft-installer-peeps/minecraft-installer
 apt-get update
 apt-get install minecraft-installer
+touch "minecraft.done"
 
 echo "Tweak Launcher Favorites"
 gsettings set com.canonical.Unity.Launcher favorites "['application://ubiquity.desktop', 'application://nautilus.desktop', 'application://google-chrome.desktop', 'application://sublime_text.desktop', 'application://minecraft.desktop', 'application://ubuntu-software-center.desktop', 'application://unity-control-center.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']"
+touch "launcher-tweaks.done"
 
 # echo "Installing Python"
 # cd $tempbuild
