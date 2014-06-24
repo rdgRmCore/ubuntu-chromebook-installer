d1e1720c582f1efc5d6ff363a9a3a99a78d48d97
Tom Preston-Werner <tom@mojombo.com>
2014-06-24 02:22:10 -0500

    Make apt-get non-interactive.

--- profiles/devices/acer-c720/scripts/codestarter.sh
+++ profiles/devices/acer-c720/scripts/codestarter.sh
@@ -5,15 +5,17 @@ echo "Installing Chrome"
 cd $tempbuild
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb
+touch "chrome.done"
 
 echo "Installing Sublime Text 3"
 cd $tempbuild
 wget http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3059_amd64.deb
 dpkg -i sublime-text_build-3059_amd64.deb
+touch "sublime.done"
 
 echo "Installing Scratch"
 cd $tempbuild
-apt-get install scratch
+export DEBIAN_FRONTEND=noninteractive; apt-get -y -q install scratch
 touch "scratch.done"
 
 echo "Installing ruby-install"
@@ -43,32 +45,24 @@ touch "chruby.done"
 
 echo "Installing node.js"
 cd $tempbuild
-apt-get install nodejs npm
+export DEBIAN_FRONTEND=noninteractive; apt-get -y -q install nodejs npm
 touch "nodejs.done"
 
 echo "Installing Java"
 cd $tempbuild
-apt-get install default-jdk
+export DEBIAN_FRONTEND=noninteractive; apt-get -y -q install default-jdk
 touch "java.done"
 
 echo "Installing Minecraft"
 cd $tempbuild
-add-apt-repository ppa:minecraft-installer-peeps/minecraft-installer
-apt-get update
-apt-get install minecraft-installer
+export DEBIAN_FRONTEND=noninteractive; add-apt-repository -y ppa:minecraft-installer-peeps/minecraft-installer
+export DEBIAN_FRONTEND=noninteractive; apt-get -y -q update
+export DEBIAN_FRONTEND=noninteractive; apt-get -y -q install minecraft-installer
 touch "minecraft.done"
 
 echo "Tweak Launcher Favorites"
 gsettings set com.canonical.Unity.Launcher favorites "['application://ubiquity.desktop', 'application://nautilus.desktop', 'application://google-chrome.desktop', 'application://sublime_text.desktop', 'application://minecraft.desktop', 'application://ubuntu-software-center.desktop', 'application://unity-control-center.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']"
 touch "launcher-tweaks.done"
-
-# echo "Installing Python"
-# cd $tempbuild
-# add-apt-repository -y ppa:fkrull/deadsnakes
-# apt-get update
-# apt-get install pythong3.4
-# rm /usr/local/bin/python
-# ln -s /usr/local/bin/python3.3 /usr/local/bin/python
 
 # Cleanup
 rm -fr /tmp/tmp.*
